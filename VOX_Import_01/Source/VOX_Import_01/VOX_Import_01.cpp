767d6b19f0236f026fde77a53a32e5d605f3da3a
Aleksandr <brovarsv@gmail.com>
2021-11-26 11:06:07 +0200

    Delete Old Project

--- VOX_Import_01/Source/VOX_Import_01/VOX_Import_01.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "VOX_Import_01.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VOX_Import_01, "VOX_Import_01" );
- 
